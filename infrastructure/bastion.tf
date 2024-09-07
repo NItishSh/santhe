bd5f162df72d3f2445902794c5c14cd79a181ea7
Nitish S Hanchinagoudra <nitish.hanchinagoudra@network.global>
2024-09-07 15:05:57 +0530

    add cert manager, external dns to eks cluster and add bastion

---
+++ infrastructure/bastion.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  subnet_id     = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [
+    aws_security_group.bastion_sg.id,
+    module.security_group.security_group_id
+  ]
+
+  tags = merge(local.common_tags, { Name = "${local.name}-bastion" })
+  lifecycle {
+    ignore_changes = [instance_state]
+  }
+  depends_on = [module.vpc]
+}
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${local.name}-bastion-sg"
+  description = "Security group for bastion host"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = ["0.0.0.0/0"] # need to change this to specific ip/32 
+    },
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = [module.vpc.vpc_cidr_block]
+    },
+  ]
+
+  tags = local.common_tags
+}
--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -6,10 +6,12 @@ module "eks_al2023" {
 
   # EKS Addons
   cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+    coredns = {}
+    # eks-pod-identity-agent = {}
+    kube-proxy   = {}
+    vpc-cni      = {}
+    cert_manager = {}
+    external_dns = {}
   }
 
   vpc_id                   = module.vpc.vpc_id
@@ -49,4 +51,26 @@ module "eks_al2023" {
   }
 
   tags = local.common_tags
+}
+module "cert_manager" {
+  source = "git::https://github.com/DNXLabs/terraform-aws-eks-cert-manager.git"
+
+  enabled = true
+
+  cluster_name                     = module.eks_al2023.cluster_id
+  cluster_identity_oidc_issuer     = module.eks_al2023.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks_al2023.oidc_provider_arn
+
+  dns01 = [
+    {
+      name           = "letsencrypt-prod"
+      namespace      = "default"
+      kind           = "ClusterIssuer"
+      dns_zone       = local.domain
+      region         = var.region
+      secret_key_ref = "letsencrypt-prod"
+      acme_server    = "https://acme-v02.api.letsencrypt.org/directory"
+      acme_email     = "your@email.com"
+    }
+  ]
 }
