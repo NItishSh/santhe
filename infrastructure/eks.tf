bfae4b7c65b4ae5feec893387d13998b5b4d1a44
Nitish S Hanchinagoudra <nitish.hanchinagoudra@network.global>
2024-09-07 22:45:46 +0530

    add trunk to this repo.

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -1,6 +1,6 @@
 module "eks_al2023" {
-  source = "terraform-aws-modules/eks/aws"
-
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.24.0"
   cluster_name    = "${local.name}-al2023"
   cluster_version = "1.30"
 
@@ -8,10 +8,10 @@ module "eks_al2023" {
   cluster_addons = {
     coredns = {}
     # eks-pod-identity-agent = {}
-    kube-proxy   = {}
-    vpc-cni      = {}
-    cert_manager = {}
-    external_dns = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+    # cert_manager = {}
+    # external_dns = {}
   }
 
   vpc_id                   = module.vpc.vpc_id
@@ -52,25 +52,25 @@ module "eks_al2023" {
 
   tags = local.common_tags
 }
-module "cert_manager" {
-  source = "git::https://github.com/DNXLabs/terraform-aws-eks-cert-manager.git"
+# module "cert_manager" {
+#   source = "git::https://github.com/DNXLabs/terraform-aws-eks-cert-manager.git"
 
-  enabled = true
+#   enabled = true
 
-  cluster_name                     = module.eks_al2023.cluster_id
-  cluster_identity_oidc_issuer     = module.eks_al2023.cluster_oidc_issuer_url
-  cluster_identity_oidc_issuer_arn = module.eks_al2023.oidc_provider_arn
+#   cluster_name                     = module.eks_al2023.cluster_id
+#   cluster_identity_oidc_issuer     = module.eks_al2023.cluster_oidc_issuer_url
+#   cluster_identity_oidc_issuer_arn = module.eks_al2023.oidc_provider_arn
 
-  dns01 = [
-    {
-      name           = "letsencrypt-prod"
-      namespace      = "default"
-      kind           = "ClusterIssuer"
-      dns_zone       = local.domain
-      region         = var.region
-      secret_key_ref = "letsencrypt-prod"
-      acme_server    = "https://acme-v02.api.letsencrypt.org/directory"
-      acme_email     = "your@email.com"
-    }
-  ]
-}
+#   dns01 = [
+#     {
+#       name           = "letsencrypt-prod"
+#       namespace      = "default"
+#       kind           = "ClusterIssuer"
+#       dns_zone       = local.domain
+#       region         = var.region
+#       secret_key_ref = "letsencrypt-prod"
+#       acme_server    = "https://acme-v02.api.letsencrypt.org/directory"
+#       acme_email     = "your@email.com"
+#     }
+#   ]
+# }
